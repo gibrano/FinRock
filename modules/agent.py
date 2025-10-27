eea26abc34acf011eeb376d3997ca3cc0a340b37
gibrangabriel.otazo <gibrangabriel.otazo@bbva.com>
2025-10-27 15:06:31 -0600

    modularize newsapi and agent

---
+++ modules/agent.py
@@ -0,0 +1,123 @@
+import requests
+from newspaper import Article
+from datetime import datetime, timedelta
+import os
+import json
+
+from openai import OpenAI
+
+import numpy as np
+
+
+class FinRock:
+    def __init__(self, news_content):
+        os.environ['OPENAI_API_KEY'] = "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"
+        self.PROMPT = '''
+                        You are a financial analyst AI specialized in understanding global news 
+                        and its potential impact on both **market sectors** and **specific stocks** 
+                        within those sectors.
+                        
+                        TASK:
+                        Analyze the following news and determine whether it could affect the stock price 
+                        of any of the target sectors or specific stocks listed below.
+                        
+                        For each **affected sector**, identify which **stocks within it** are most likely to be impacted.  
+                        Then, for each affected sector, classify the overall expected price movement as 
+                        "Up", "Down", or "No Change", and briefly explain why.
+                        
+                        If a sector and its stocks are not meaningfully affected, mark the sector as "unaffected."
+                        
+                        NEWS:
+                        "{news_text}"
+                        
+                        TARGET SECTORS AND STOCKS:
+                        {sector_stock_mapping}
+                        
+                        OUTPUT FORMAT (strict JSON):
+                        {{
+                          "news_headline": "<headline or short summary>",
+                          "affected_sectors": [
+                            {{
+                              "sector": {{
+                                "<sector_name>": ["<stock_1>", "<stock_2>", ...]
+                              }},
+                              "stock_price_effect": "<Up|Down|No Change>",
+                              "reasoning": "<short explanation>"
+                            }}
+                          ],
+                          "unaffected_sectors": ["<list of sectors not affected>"]
+                        }}
+                        '''
+        self.data = news_content
+        self.client = OpenAI()
+        
+    def save(self):
+        pass
+
+    def get_sectors(self):
+        with open("data/sectors.json", "r") as f:
+            sectors = json.load(f)
+        return sectors
+
+    def analize_news(self, news, sectors2stocks):
+        
+        prompt = self.PROMPT.format(
+            news_text=news,
+            sector_stock_mapping=sectors2stocks
+        )
+
+    
+        input=[
+                {
+                    "role": "system",
+                    "content": "You are a financial analyst AI specialized in understanding global news "
+                               "and its potential impact on both **market sectors** and **specific stocks**" 
+                               "within those sectors. Always return results in strict JSON."
+                },
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+        ]
+    
+        
+    
+        response = self.client.responses.create(
+            model="gpt-5",
+            input=input,
+            reasoning={
+                "effort": "medium"
+            }
+        )
+        
+        out = response.output[1].content[0].text
+        out = json.loads(out.replace("\\", "\\\\"))
+        return out
+        
+    def get_insights(self):
+        results = []
+        stocks2sectors = self.get_sectors()
+
+        sectors2stocks = {}
+        for k, v in stocks2sectors.items():
+            sectors2stocks[v] = sectors2stocks.get(v, []) + [k]
+
+        affected = {}
+
+        for sector in np.unique(list(stocks2sectors.values())):
+            affected[sector] = {'Up': 0, 'Down': 0, 'No Change': 0}
+        
+        N = len(self.data["articles"])
+        for i in range(N):
+            out = self.analize_news(self.data["articles"][i]['full_content'], sectors2stocks) #", ".join(np.unique(list(sectors.values())))
+            
+            out['url'] = self.data["articles"][i]['url']
+            results.append(out)
+            print(out)
+            print("="*80)
+
+            for obj in out["affected_sectors"]:
+                sector = list(obj['sector'].keys())[0]
+                affected[sector][obj['stock_price_effect']] += 1
+        
+        return results, affected  
---
+++ modules/newsapi.py
@@ -0,0 +1,78 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+import requests
+from newspaper import Article
+from datetime import datetime, timedelta
+import os
+import json
+
+class NewsCollector:
+    def __init__(self, start_date, end_date):
+        self.NEWS_API_KEY = "3884f57e128e46cba4f1a6f38eaa0027"
+        self.start_date = start_date
+        self.end_date = end_date
+
+        self.newsapi_url = (
+            f"https://newsapi.org/v2/top-headlines?"
+            f"country=us&"                       # palabra clave
+            f"from={self.start_date}&"               # desde hace 7 días
+            f"to={self.end_date}&"                     # hasta hoy
+            f"sortBy=publishedAt&"             # ordenar por fecha
+            f"language=en&"                    # idioma (puedes cambiar a 'es')
+            f"apiKey={self.NEWS_API_KEY}"
+        )
+
+    def get_page(self, url):
+        html = ""
+        options = Options()
+        options.add_argument("--headless=new")  # safer than "--headless"
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-gpu")
+        options.add_argument("--disable-dev-shm-usage")  # critical for Linux
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--disable-features=VizDisplayCompositor")
+        options.add_argument("--window-size=1920,1080")
+        options.page_load_strategy = 'eager'  # don't wait for full JS load
+
+        driver = webdriver.Chrome(options=options)
+        chrome_options = webdriver.ChromeOptions()
+        #driver.set_page_load_timeout(30)
+        driver.implicitly_wait(10)
+        driver.set_window_size(1920, 1080)
+        try:
+            driver.get(url)            
+            html = driver.page_source
+        except Exception as e:
+            print("Timeout:", e)
+        finally:
+            driver.quit()
+        return html
+
+    def get_all_news(self):
+        response = requests.get(self.newsapi_url)
+        return response.json()    
+
+    
+    def collect(self):
+        self.data = self.get_all_news()
+
+        N = len(self.data["articles"])
+        for i in range(N):            
+            article = self.data["articles"][i]
+            url = article["url"]
+            print(i, "portal --->",url)
+            web_page = self.get_page(url)
+            print("content --->",web_page[:20])
+            news_article = Article(url="")
+            news_article.set_html(web_page)
+            news_article.parse()
+
+            self.data['articles'][i]['full_content'] = (str(self.data['articles'][i]['title'] or "") 
+                                                        + "\n" + str(self.data['articles'][i]['content'] or "") 
+                                                        + "\n" 
+                                                        + str(news_article.text or ""))
